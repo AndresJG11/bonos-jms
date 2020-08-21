e9a116637109a1f6988aa1ab30f397ee7afc16a7
Andres JG <anjimenez11@hotmail.com>
2020-08-21 18:54:34 -0500

    build

--- src/App.js
+++ src/App.js
@@ -5,9 +5,10 @@ import { BrowserRouter as Router, Switch, Route, HashRouter } from 'react-router
 import { Usuario } from './components/Usuario';
 
 function App() {
-
+  
+  console.log("This is the process.env", process.env.PUBLIC_URL)
   return (
-    <HashRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter basename={'https://andresjg11.github.io/bonos-jms/#/'}>
       <div className="App">
         <Router>
           <Switch>

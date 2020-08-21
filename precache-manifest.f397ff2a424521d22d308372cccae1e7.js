4d00e37b7e8aaf490669ad1fb46c61772b0d62bd
Andres JG <anjimenez11@hotmail.com>
2020-08-21 18:20:42 -0500

    Updates

---
+++ precache-manifest.f397ff2a424521d22d308372cccae1e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e55a86e4e31a7e4acd2c111f7251900",
+    "url": "//andresjg11.github.io/bonos-jms/index.html"
+  },
+  {
+    "revision": "6d4145ce52a7516a2f8f",
+    "url": "//andresjg11.github.io/bonos-jms/static/css/main.5c33d501.chunk.css"
+  },
+  {
+    "revision": "dd1b562051dbf39b7718",
+    "url": "//andresjg11.github.io/bonos-jms/static/js/2.c2fa328d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "//andresjg11.github.io/bonos-jms/static/js/2.c2fa328d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d4145ce52a7516a2f8f",
+    "url": "//andresjg11.github.io/bonos-jms/static/js/main.b2e5e562.chunk.js"
+  },
+  {
+    "revision": "81e97011a1d411181e9a",
+    "url": "//andresjg11.github.io/bonos-jms/static/js/runtime-main.0dd002aa.js"
+  }
+]);
